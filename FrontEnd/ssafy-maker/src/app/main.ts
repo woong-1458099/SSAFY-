8c3ab8a88e06fc2faa2308f6ac9e4a32f08db0ea
김명진 <usbdhffo112@naver.com>
2026-03-23 16:44:00 +0900

    build: 환경변수 검증 체계 정비

--- FrontEnd/ssafy-maker/src/app/main.ts
+++ FrontEnd/ssafy-maker/src/app/main.ts
@@ -1,7 +1,9 @@
 import { createGame } from "./game";
+import { assertAppEnv } from "./config/env";
 import { initializeAuthGateway } from "../features/auth/AuthGateway";
 
 async function bootstrapApplication(): Promise<void> {
+  assertAppEnv();
   const authBootstrap = await initializeAuthGateway();
   createGame(authBootstrap);
 }
