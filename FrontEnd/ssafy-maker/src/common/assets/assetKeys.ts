b19bcb196519f06fd989f5b5d377ec0e70eef29b
최연웅 <yunwoong2@gmail.com>
2026-03-24 09:45:55 +0900

    login 수정

--- FrontEnd/ssafy-maker/src/common/assets/assetKeys.ts
+++ FrontEnd/ssafy-maker/src/common/assets/assetKeys.ts
@@ -30,18 +30,18 @@ export const ASSET_KEYS = {
 
 export const ASSET_PATHS = {
   ui: {
-    buttons: "assets/game/ui/buttons.png"
+    buttons: "/assets/game/ui/buttons.png"
   },
   map: {
-    tilesetImage: "assets/game/map/FullAsset.png",
-    tilesetTsx: "assets/game/map/FullTileSet.tsx",
-    worldTmx: "assets/game/map/mainMap.tmx",
-    downtownTmx: "assets/game/map/city.tmx",
-    campusTmx: "assets/game/map/inSSAFY.tmx"
+    tilesetImage: "/assets/game/map/FullAsset.png",
+    tilesetTsx: "/assets/game/map/FullTileSet.tsx",
+    worldTmx: "/assets/game/map/mainMap.tmx",
+    downtownTmx: "/assets/game/map/city.tmx",
+    campusTmx: "/assets/game/map/inSSAFY.tmx"
   },
   story: {
-    authoredDialogues: "assets/game/data/story/authored/dialogues.json",
-    authoredSceneStates: "assets/game/data/story/authored/scene_states.json"
+    authoredDialogues: "/assets/game/data/story/authored/dialogues.json",
+    authoredSceneStates: "/assets/game/data/story/authored/scene_states.json"
   }
 } as const;
 
